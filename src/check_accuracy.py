5720e085e40ecc54f877025a1fc15c04ea69d9ae
Thilak Acharya <151428944+Zoroonepice@users.noreply.github.com>
2026-06-04 10:29:22 +0530

    Update check_accuracy.py

--- src/check_accuracy.py
+++ src/check_accuracy.py
@@ -1,1 +1,23 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
+# Load model
+model = tf.keras.models.load_model("soil_model_densenet.h5")
+
+# Dataset path
+test_path = "Dataset/test"
+
+# Preprocess test data
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+test_data = test_datagen.flow_from_directory(
+    test_path,
+    target_size=(224,224),
+    batch_size=8,
+    class_mode='categorical'
+)
+
+# Evaluate model
+loss, accuracy = model.evaluate(test_data)
+
+print("Test Accuracy:", accuracy)
